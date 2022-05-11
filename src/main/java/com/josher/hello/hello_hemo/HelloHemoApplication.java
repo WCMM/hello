5aa4b90156b090d53eb911fdad66596a74b79553
wangnan <424351706@qq.com>
2022-05-11 22:15:51 +0800

    项目初始化

---
+++ src/main/java/com/josher/hello/hello_hemo/HelloHemoApplication.java
@@ -0,0 +1,13 @@
+package com.josher.hello.hello_hemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloHemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/josher/hello/hello_hemo/HelloHemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josher.hello.hello_hemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
