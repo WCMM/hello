44a47837aa1e6c1ab08d0b00ef551260b9b2defd
wangnan <424351706@qq.com>
2022-05-11 23:01:28 +0800

    增加学生类

---
+++ src/main/java/com/josher/hello/hello_hemo/pojo/Student.java
@@ -0,0 +1,15 @@
+package com.josher.hello.hello_hemo.pojo;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private String name;
+
+    private Integer age;
+
+    private Integer gender;
+
+    private String address;
+}
